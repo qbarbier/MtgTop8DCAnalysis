e773237bf5124a3df251a228fd881af4831249ef
Quentin <quentin.barbier89@gmail.com>
2023-08-19 22:31:12 +0200

    Update mtgtop8_DC_compute_scraping.R
    Convert all script in to 3 functions

--- mtgtop8_DC_compute_scraping.R
+++ mtgtop8_DC_compute_scraping.R
@@ -1,45 +1,56 @@
-load("mtgtop8DC.Rdata")
-
-db.list <- list()
-for(i in db){
-  res <- lapply(c(1:length(i$decks)),function(j){
-    return(list(
-      "decks"=i$decks[[j]],
-      "date"=i$date,
-      "position"=names(i$decks)[[j]],
-      "players"=i$players,
-      "top.players"=length(i$decks)
-    ))
-  })
-  db.list[[length(db.list)+1]]<-do.call(rbind, res)
+########## CONVERT MTGTOP8 DATA #######################################
+#'@title Convert MTGtop8 Rdata output to Dataframe
+#'@description Convert output of scraping function to daframe for computing
+#'@author Quentin Barbier
+#'
+convert.mtgtop8.to.df <- function(db){
+  db.list <- list()
+  for(i in db){
+    res <- lapply(c(1:length(i$decks)),function(j){
+      return(list(
+        "decks"=i$decks[[j]],
+        "date"=i$date,
+        "position"=names(i$decks)[[j]],
+        "players"=i$players,
+        "top.players"=length(i$decks)
+      ))
+    })
+    db.list[[length(db.list)+1]]<-do.call(rbind, res)
+  }
+  
+  mat <- data.frame(do.call(rbind, db.list))
+  return(mat)
 }
 
-mat <- data.frame(do.call(rbind, db.list))
-
-ronde.suisse <- list(
-  "5-8"=3,"9-16"=4,"17-32"=5,"33-64"=6,
-  "65-128"=7,"129-226"=8,"227-409"=9
-)
-pretop8.score <- list("3"=3,
-                      "4"=3,"5"=3,"6"=4,"7"=5,"8"=6,"9"=7
-)
-posttop8.score <- data.frame(
-  "1"=c(0,1,2,2,2,2,3,3),
-  "2"=c(0,0,1,1,2,2,2,2),
-  "3-4"=c(0,0,0,1,1,1,1,1),
-  "3"=c(0,0,0,1,1,1,1,1),
-  "4"=c(0,0,0,0,1,1,1,1),
-  "5-8"=c(0,0,0,0,0,1,1,1),
-  "5"=c(0,0,0,0,0,1,1,1),
-  "6"=c(0,0,0,0,0,0,1,1),
-  "7"=c(0,0,0,0,0,0,0,1),
-  "8"=c(0,0,0,0,0,0,0,0)
-)
-posttop8.score <- t(posttop8.score)
-row.names(posttop8.score) <- c("1","2","3-4","3","4","5-8","5","6","7","8")
-colnames(posttop8.score) <- c("1","2","3","4","5","6","7","8")
-
-mtgtop8.compute.score <- function(mat,ronde.suisse,pretop8.score,posttop8.score){
+########## MTGTOP8 ESTIMATE WINRATE #######################################
+#'@title Estime win rate of mtgtop8 data
+#'@author Quentin Barbier
+#'@description  
+#'
+mtgtop8.estimate.winrate <- function(mat){
+  
+  ronde.suisse <- list(
+    "5-8"=3,"9-16"=4,"17-32"=5,"33-64"=6,
+    "65-128"=7,"129-226"=8,"227-409"=9
+  )
+  pretop8.score <- list("3"=3,
+                        "4"=3,"5"=3,"6"=4,"7"=5,"8"=6,"9"=7
+  )
+  posttop8.score <- data.frame(
+    "1"=c(0,1,2,2,2,2,3,3),
+    "2"=c(0,0,1,1,2,2,2,2),
+    "3-4"=c(0,0,0,1,1,1,1,1),
+    "3"=c(0,0,0,1,1,1,1,1),
+    "4"=c(0,0,0,0,1,1,1,1),
+    "5-8"=c(0,0,0,0,0,1,1,1),
+    "5"=c(0,0,0,0,0,1,1,1),
+    "6"=c(0,0,0,0,0,0,1,1),
+    "7"=c(0,0,0,0,0,0,0,1),
+    "8"=c(0,0,0,0,0,0,0,0)
+  )
+  posttop8.score <- t(posttop8.score)
+  row.names(posttop8.score) <- c("1","2","3-4","3","4","5-8","5","6","7","8")
+  colnames(posttop8.score) <- c("1","2","3","4","5","6","7","8")
   
   win.rate <- function(players,top.players,ronde.suisse,position){
     res <- lapply(c(1:length(ronde.suisse)),function(i){
@@ -89,7 +100,43 @@ mtgtop8.compute.score <- function(mat,ronde.suisse,pretop8.score,posttop8.score)
   
 }
 
-mat <- mtgtop8.compute.score(mat,ronde.suisse,pretop8.score,posttop8.score)
+
+########## MTGTOP8 ARCHEYTPE #############################
+#'@title
+#'
+#'
+#'
+mtgtop8.archetype <- function(mat, archetype, type=NULL){
+  group <- list()
+  for(i in c(1:dim(mat)[1])){
+    d.name <- mat[i,'decks']
+    id <- which(lapply(archetype, function(k){grep(k, d.name)})==1)
+    if(length(id)!=1){
+      g <- NA
+    } else {
+      g <- names(archetype)[id]
+    }
+    group[[length(group)+1]] <- g
+  }
+  group <- unlist(group)
+  mat <- cbind(mat, group)
+  
+  win.rate <- apply(mat[,c(6,7)],1, function(x){
+    return((x[[1]]/x[[2]])*100)
+  })
+  mat <- cbind(mat, win.rate)
+  
+  mat$date <- as.Date(unlist(mat$date),"%d/%m/%y")
+  mat$players <- unlist(mat$players)
+  return(mat)
+}
+
+
+########## RUN SCRIPT #####
+
+load("mtgtop8DC.Rdata")
+mat <- convert.mtgtop8.to.df(db)
+mat <- mtgtop8.estimate.winrate(mat)
 
 archetype  <- list(
   "Grist"=".*Grist.*",
@@ -126,26 +173,5 @@ archetype  <- list(
   "Octavia"=".*Octavia.*"
 )
 
-group <- list()
-for(i in c(1:dim(mat)[1])){
-  d.name <- mat[i,'decks']
-  id <- which(lapply(archetype, function(k){grep(k, d.name)})==1)
-  if(length(id)!=1){
-    g <- "Other"
-  } else {
-    g <- names(archetype)[id]
-  }
-  group[[length(group)+1]] <- g
-}
-group <- unlist(group)
-mat <- cbind(mat, group)
-
-win.rate <- apply(mat[,c(6,7)],1, function(x){
-  return((x[[1]]/x[[2]])*100)
-})
-mat <- cbind(mat, win.rate)
-
-mat$date <- as.Date(unlist(mat$date),"%d/%m/%y")
-mat$players <- unlist(mat$players)
-
+mat <- mtgtop8.archetype(mat, archetype, type=NULL)
 save(mat, file="mtgtop8DC_computeTable.Rdata")
